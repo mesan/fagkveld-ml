b97ba5dc01dbc984f319f29965b5a2170c1e1dc5
Petter F. Asla <pettera@mesan.no>
2017-02-15 21:38:03 +0100

    Peke mot oppgave 5

--- src/main/java/com/netcompany/machinelearning/nevraltNettverk/Oppgaver.java
+++ src/main/java/com/netcompany/machinelearning/nevraltNettverk/Oppgaver.java
@@ -71,6 +71,7 @@ public class Oppgaver {
         // OPPGAVE 5: Implementer app
         // ###############################################################
 
+        // Se under SifferKlassifikator
     }
 
 
