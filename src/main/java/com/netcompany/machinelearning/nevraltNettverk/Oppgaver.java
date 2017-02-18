13236bf060b9a9023eef27b47f4ecc75e0ed533d
okrepp <okrepp91@gmail.com>
2017-02-18 14:35:52 +0100

    Legger til lf for oppgave 1. Klargjør for oppgave 2.

--- src/main/java/com/netcompany/machinelearning/nevraltNettverk/Oppgaver.java
+++ src/main/java/com/netcompany/machinelearning/nevraltNettverk/Oppgaver.java
@@ -20,8 +20,12 @@ public class Oppgaver {
         // ###############################################################
 
         // OPPG 1 a) Flat ut bilder
+        int[][] flateTreningsbilder = flatUtBilder(dataLaster.getTreningsbilder());
+        int[][] flateTestbilder = flatUtBilder(dataLaster.getTestbilder());
 
         // OPPG 1 b) Konverter pikselverdiene fra [0, 255] til [-1, 1])
+        double[][] normaliserteTreningsbilder = normaliser(flateTreningsbilder);
+        double[][] normaliserteTestbilder = normaliser(flateTestbilder);
 
         // ###############################################################
         // TODO: OPPGAVE 2: NEVRALT NETTVERK -> BYGG NETTVERK
@@ -54,14 +58,40 @@ public class Oppgaver {
     // Metodene under kan være til hjelp for oppgave 1:
 
     private static int[][] flatUtBilder(final int[][][] bilder) {
-        return new int[0][];
+        final int numberOfImages = bilder.length;
+        final int[][] flateBilder = new int[numberOfImages][bilder[0].length * bilder[0][0].length];
+
+        for (int i = 0; i < numberOfImages; i++) {
+            flateBilder[i] = flatUtEttBilde(bilder[i]);
+        }
+        return flateBilder;
     }
 
     private static int[] flatUtEttBilde(final int[][] bilde) {
-        return new int[0];
+        int pikselindeks = 0;
+        final int[] flattBilde = new int[bilde.length * bilde[0].length];
+        for (final int[] rad : bilde) {
+            for (final int piksel : rad) {
+                flattBilde[pikselindeks] = piksel;
+                pikselindeks++;
+            }
+        }
+        return flattBilde;
     }
 
     private static double[][] normaliser(final int[][] bilder) {
-        return new double[0][];
+        final int antallBilder = bilder.length;
+        final int antallPiksler = bilder[0].length;
+
+        final double[][] normaliserteBilder = new double[antallBilder][antallPiksler];
+
+        final double MAX_PIXEL_VERDI = 255.0; // FORDI VI VET DET :)
+
+        for (int bildeIndeks = 0; bildeIndeks < antallBilder; bildeIndeks++) {
+            for (int piksel = 0; piksel < antallPiksler; piksel++) {
+                normaliserteBilder[bildeIndeks][piksel] = (bilder[bildeIndeks][piksel] / MAX_PIXEL_VERDI) * 2 - 1;
+            }
+        }
+        return normaliserteBilder;
     }
 }
