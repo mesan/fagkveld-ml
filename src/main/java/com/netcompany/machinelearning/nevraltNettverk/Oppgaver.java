41a8681109c5e1922818f74818f044977f6bfc29
okrepp <okrepp91@gmail.com>
2017-02-18 13:53:13 +0100

    Fjerner "LF" og gjør alt klart for oppgave 1.

--- src/main/java/com/netcompany/machinelearning/nevraltNettverk/Oppgaver.java
+++ src/main/java/com/netcompany/machinelearning/nevraltNettverk/Oppgaver.java
@@ -11,108 +11,57 @@ public class Oppgaver {
 
     public static void main(final String[] args) throws Exception {
 
-       /* // Laster inn bilder. Velg om du vil bruke hele eller deler av datasettet.
+        // Laster inn bilder. Velg om du vil bruke hele eller deler av datasettet.
         final Boolean lesInnHeleDatasettet = false;
         final DataLaster dataLaster = new DataLaster(lesInnHeleDatasettet);
 
         // ###############################################################
-        // OPPGAVE 1: PREPROSESSERING -> FLAT UT OG NORMALISER
+        // TODO: OPPGAVE 1: PREPROSESSERING -> FLAT UT OG NORMALISER
         // ###############################################################
 
         // OPPG 1 a) Flat ut bilder
-        int[][] flateTreningsbilder = flatUtBilder(dataLaster.getTreningsbilder());
-        int[][] flateTestbilder = flatUtBilder(dataLaster.getTestbilder());
 
         // OPPG 1 b) Konverter pikselverdiene fra [0, 255] til [-1, 1])
-        double[][] normaliserteTreningsbilder = normaliser(flateTreningsbilder);
-        double[][] normaliserteTestbilder = normaliser(flateTestbilder);
-
 
         // ###############################################################
-        // OPPGAVE 2: NEVRALT NETTVERK -> BYGG NETTVERK
+        // TODO: OPPGAVE 2: NEVRALT NETTVERK -> BYGG NETTVERK
         // ###############################################################
 
         // Bruk NevralNettverkBygger til å opprette ett nettverk.
         // For nettverkslagene: husk at hvert input-lag må ha samme antall noder som forrige output-laget
 
-        final NevraltNettverk nevraltNettverk = new NevraltNettverkBygger()
-                .leggTilLag(784, 200)
-                .leggTilLag(200, 10)
-                .bygg();
-
-        nevraltNettverk.setAntallEpoker(100);
-        nevraltNettverk.setBatchStorrelse(40);
-
         // ###############################################################
-        // OPPGAVE 3: TREN OG TEST
+        // TODO: OPPGAVE 3: TREN OG TEST
         // ###############################################################
 
-        nevraltNettverk.tren(normaliserteTreningsbilder, dataLaster.getTreningsfasit());
-
-        nevraltNettverk.evaluer(normaliserteTestbilder, dataLaster.getTestfasit());
-
         // ###############################################################
-        // OPPGAVE 4: LEK DEG MED PARAMETRE
+        // TODO: OPPGAVE 4: LEK DEG MED PARAMETRE
         // ###############################################################
 
         // Prøv med forskjellige lag.
         // Sett forskjellige lengder på epoker.
-        // Sett forskjellige batch størrelser.
-
-        final NevraltNettverk nevraltNettverk2 = new NevraltNettverkBygger()
-                .leggTilLag(784, 400)
-                .leggTilLag(400, 50)
-                .leggTilLag(50, 20)
-                .bygg();
-
-        nevraltNettverk.setAntallEpoker(500);
-        nevraltNettverk.setBatchStorrelse(40);*/
+        // Sett forskjellige batch-størrelser.
 
         // ###############################################################
-        // OPPGAVE 5: Implementer app
+        // TODO: OPPGAVE 5: Implementer app
         // ###############################################################
 
         // Skriv kode i SifferKlassifikator
-        SifferKlassifikatorApp.start();
+        //SifferKlassifikatorApp.start();
 
     }
 
-
+    // Metodene under kan være til hjelp for oppgave 1:
 
     private static int[][] flatUtBilder(final int[][][] bilder) {
-        final int numberOfImages = bilder.length;
-        final int[][] flateBilder = new int[numberOfImages][bilder[0].length * bilder[0][0].length];
-
-        for (int i = 0; i < numberOfImages; i++) {
-            flateBilder[i] = flatUtEttBilde(bilder[i]);
-        }
-        return flateBilder;
+        return new int[0][];
     }
+
     private static int[] flatUtEttBilde(final int[][] bilde) {
-        int pikselindeks = 0;
-        final int[] flattBilde = new int[bilde.length * bilde[0].length];
-        for (final int[] rad : bilde) {
-            for (final int piksel : rad) {
-                flattBilde[pikselindeks] = piksel;
-                pikselindeks++;
-            }
-        }
-        return flattBilde;
+        return new int[0];
     }
 
     private static double[][] normaliser(final int[][] bilder) {
-        final int antallBilder = bilder.length;
-        final int antallPiksler = bilder[0].length;
-
-        final double[][] normaliserteBilder = new double[antallBilder][antallPiksler];
-
-        final double MAX_PIXEL_VERDI = 255.0; // FORDI VI VET DET :)
-
-        for (int bildeIndeks = 0; bildeIndeks < antallBilder; bildeIndeks++) {
-            for (int piksel = 0; piksel < antallPiksler; piksel++) {
-                normaliserteBilder[bildeIndeks][piksel] = (bilder[bildeIndeks][piksel] / MAX_PIXEL_VERDI) * 2 - 1;
-            }
-        }
-        return normaliserteBilder;
+        return new double[0][];
     }
 }
