91a8b673d93012a312d95ecd5042542db0e143f9
okrepp <okrepp91@gmail.com>
2017-02-18 15:24:24 +0100

    Rydding.

--- src/main/java/com/netcompany/machinelearning/app/SifferKlassifikator.java
+++ src/main/java/com/netcompany/machinelearning/app/SifferKlassifikator.java
@@ -13,7 +13,6 @@ class SifferKlassifikator {
         /* Oppgave 5: Bytt ut den heller dårlige random-klassifikatoren under.
 
         Hint: Tren et nevralt nettverk å få modellen inn her på en eller annen måte.
-        Hint: Valgt fil er tilgjengelig i valgtFil
         Hint: DataHjelper har en bildeTilIntArray-metode som sannsynligvis vil være til hjelp*/
 
         return (Integer) (int) (Math.random() * 10);
