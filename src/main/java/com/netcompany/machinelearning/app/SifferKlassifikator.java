1621072fd9942f514f4c3a8187ac43307ac24563
Petter Fagerlund Asla <pettera@netcompay.com>
2017-10-30 09:55:56 +0100

    La til noe mer i hintene på opgpave5

--- src/main/java/com/netcompany/machinelearning/app/SifferKlassifikator.java
+++ src/main/java/com/netcompany/machinelearning/app/SifferKlassifikator.java
@@ -13,7 +13,14 @@ class SifferKlassifikator {
         /* Oppgave 5: Bytt ut den heller dårlige random-klassifikatoren under.
 
         Hint: Tren et nevralt nettverk å få modellen inn her på en eller annen måte.
-        Hint: DataHjelper har en bildeTilIntArray-metode som sannsynligvis vil være til hjelp*/
+              Nettverket kan f.eks bygges og trenes i konstruktøren, eller legge inn alt i denne metoden.
+              Bruk det du har gjort i oppg 1-4 her.
+
+        Hint: DataHjelper har en bildeTilIntArray-metode som sannsynligvis vil være til hjelp.
+              Bruk hjelpeklassen DataHjelper når du skal lese inn et bilde.
+              Deretter flat ut og normaliser som gjort tidligere. Til slutt, prediker bilde du leste inn.
+
+        */
 
         return (Integer) (int) (Math.random() * 10);
     }
